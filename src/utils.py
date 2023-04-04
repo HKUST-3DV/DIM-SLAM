9adcde63af8ae2ebfa97b68300044eb7df005a11
hengli <github@hengli.me>
2023-04-04 13:11:12 +0000

    code_release: remove mathutils

--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,7 @@
 from src.eval_pose import evaluate_ate
 import torch
 import numpy as np
-from mathutils import Matrix
+# from mathutils import Matrix
 
 def evaluate(poses_gt, poses_est, plot=None):
 
@@ -288,11 +288,12 @@ def get_tensor_from_camera(RT, Tquad=False, method="quat"):
             RT = RT.detach().cpu()
             gpu_id = RT.get_device()
         RT = RT.numpy()
-    from mathutils import Matrix
+    # from mathutils import Matrix
     R, T = RT[:3, :3], RT[:3, 3]
     if method == "quat":
-        rot = Matrix(R)
-        quad = rot.to_quaternion()
+        # rot = Matrix(R)
+        # quad = rot.to_quaternion()
+        quad = rot2quaternion(torch.from_numpy(R).unsqueeze(0)).squeeze(0).detach().cpu().numpy()
     elif method == "ortho6d":
         quad = compute_ortho6d_from_rotation_matrix(R)
     else:
@@ -330,6 +331,76 @@ def quad2rotation(quad):
     rot_mat[:, 2, 2] = 1 - two_s * (qi ** 2 + qj ** 2)
     return rot_mat
 
+
+def rot2quaternion(rot_mat, eps=1e-6):
+    """ Convert 3x4 rotation matrix to 4d quaternion vector [From torchgeometrc library]
+    This algorithm is based on algorithm described in
+    https://github.com/KieranWynn/pyquaternion/blob/master/pyquaternion/quaternion.py#L201
+    Args:
+        rot_mat (Tensor): the rotation matrix to convert.
+    Return:
+        Tensor: the rotation in quaternion
+    Shape:
+        - Input: :math:`(N, 3, 4)`
+        - Output: :math:`(N, 4)`
+    Example:
+        >>> input = torch.rand(4, 3, 4)  # Nx3x4
+        >>> output = tgm.rot_mat_to_quaternion(input)  # Nx4
+    """
+    if not torch.is_tensor(rot_mat):
+        raise TypeError("Input type is not a torch.Tensor. Got {}".format(
+            type(rot_mat)))
+
+    if len(rot_mat.shape) > 3:
+        raise ValueError(
+            "Input size must be a three dimensional tensor. Got {}".format(
+                rot_mat.shape))
+
+    rot_mat = rot_mat[:, :3, :3]
+    rmat_t = torch.transpose(rot_mat, 1, 2)
+
+    mask_d2 = (rmat_t[:, 2, 2] < eps).float()
+    mask_d0_d1 = (rmat_t[:, 0, 0] > rmat_t[:, 1, 1]).float()
+    mask_d0_nd1 = (rmat_t[:, 0, 0] < -rmat_t[:, 1, 1]).float()
+
+    t0 = 1 + rmat_t[:, 0, 0] - rmat_t[:, 1, 1] - rmat_t[:, 2, 2]
+    q0 = torch.stack([rmat_t[:, 1, 2] - rmat_t[:, 2, 1],
+                      t0, rmat_t[:, 0, 1] + rmat_t[:, 1, 0],
+                      rmat_t[:, 2, 0] + rmat_t[:, 0, 2]], -1)
+    t0_rep = t0.repeat(4, 1).t()
+
+    t1 = 1 - rmat_t[:, 0, 0] + rmat_t[:, 1, 1] - rmat_t[:, 2, 2]
+    q1 = torch.stack([rmat_t[:, 2, 0] - rmat_t[:, 0, 2],
+                      rmat_t[:, 0, 1] + rmat_t[:, 1, 0],
+                      t1, rmat_t[:, 1, 2] + rmat_t[:, 2, 1]], -1)
+    t1_rep = t1.repeat(4, 1).t()
+
+    t2 = 1 - rmat_t[:, 0, 0] - rmat_t[:, 1, 1] + rmat_t[:, 2, 2]
+    q2 = torch.stack([rmat_t[:, 0, 1] - rmat_t[:, 1, 0],
+                      rmat_t[:, 2, 0] + rmat_t[:, 0, 2],
+                      rmat_t[:, 1, 2] + rmat_t[:, 2, 1], t2], -1)
+    t2_rep = t2.repeat(4, 1).t()
+
+    t3 = 1 + rmat_t[:, 0, 0] + rmat_t[:, 1, 1] + rmat_t[:, 2, 2]
+    q3 = torch.stack([t3, rmat_t[:, 1, 2] - rmat_t[:, 2, 1],
+                      rmat_t[:, 2, 0] - rmat_t[:, 0, 2],
+                      rmat_t[:, 0, 1] - rmat_t[:, 1, 0]], -1)
+    t3_rep = t3.repeat(4, 1).t()
+
+    mask_c0 = mask_d2 * mask_d0_d1
+    mask_c1 = mask_d2 * (1 - mask_d0_d1)
+    mask_c2 = (1 - mask_d2) * mask_d0_nd1
+    mask_c3 = (1 - mask_d2) * (1 - mask_d0_nd1)
+    mask_c0 = mask_c0.view(-1, 1).type_as(q0)
+    mask_c1 = mask_c1.view(-1, 1).type_as(q1)
+    mask_c2 = mask_c2.view(-1, 1).type_as(q2)
+    mask_c3 = mask_c3.view(-1, 1).type_as(q3)
+
+    q = q0 * mask_c0 + q1 * mask_c1 + q2 * mask_c2 + q3 * mask_c3
+    q /= torch.sqrt(t0_rep * mask_c0 + t1_rep * mask_c1 +  # noqa
+                    t2_rep * mask_c2 + t3_rep * mask_c3)  # noqa
+    q *= 0.5
+    return q
 
 def get_camera_from_tensor(inputs):
 
